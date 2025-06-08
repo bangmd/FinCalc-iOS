2c2860c5be18c9f88f85a685f36641a9ec67a94c
bangmd <soslan74d@gmail.com>
2025-06-08 18:08:32 +0300

    Initial commit - XCode project structure

---
+++ FinCalc-iOS/FinCalc-iOS/FinCalc_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinCalc_iOSApp.swift
+//  FinCalc-iOS
+//
+//  Created by Soslan Dzampaev on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinCalc_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
