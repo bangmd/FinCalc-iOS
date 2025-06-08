3bea4f3779fc0219ef5de8bf17ca95742fca1037
bangmd <soslan74d@gmail.com>
2025-06-08 22:42:51 +0300

    - Added Category struct - Added Direction enum - Custom decoding from JSON for isIncome and emoji

---
+++ FinCalc-iOS/FinCalc-iOS/Models/Category.swift
@@ -0,0 +1,37 @@
+//
+//  Category.swift
+//  FinCalc-iOS
+//
+//  Created by Soslan Dzampaev on 08.06.2025.
+//
+
+import Foundation
+
+enum Direction: Codable {
+    case income
+    case outcome
+}
+
+struct Category: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let emoji: Character
+    let direction: Direction
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case emoji
+        case isIncome
+    }
+    
+    init(from decoder: Decoder ) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        let emojiString = try container.decode(String.self, forKey: .emoji)
+        emoji = emojiString.first ?? "❌"
+        let isIncome = try container.decode(Bool.self, forKey: .isIncome)
+        direction = isIncome ? .income : .outcome
+    }
+}
